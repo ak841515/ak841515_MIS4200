b5857d08a66d9a59ee6f6b2bab33854b576f749a
Kiefer <ak841515@ohio.edu>
2020-09-05 22:39:49 -0400

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ak841515_MIS4200.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ak841515_MIS4200.Startup))]
+namespace ak841515_MIS4200
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
